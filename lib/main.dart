c92619101cebd2da19acc2d27e5b60c4e4148b85
mahamudsourov <sourovm554@gmail.com>
2025-02-18 10:15:06 +0600

    Adding_Icons

--- lib/main.dart
+++ lib/main.dart
@@ -10,44 +10,58 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI - FLutter",
+      title: "Basic UI - Flutter",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter App"),
           centerTitle: true,
           elevation: 10.5,
         ),
-        body: Row(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              Icons.ac_unit,
-              size: 60,
-              color: const Color.fromARGB(255, 0, 217, 255),
+            Row(
+              children: [
+                Icon(
+                  Icons.ac_unit,
+                  size: 60,
+                  color: const Color.fromARGB(255, 0, 217, 255),
+                ),
+                Text(
+                  "Hi, ",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  "This is ",
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  "Flutter ",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 30,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              "Hi, ",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 30,
-              ),
+            SizedBox(height: 20), 
+            Row(
+              children: [
+                Icon(Icons.star, color: Colors.grey, size: 30), 
+                Icon(Icons.star, color: Colors.grey, size: 30), 
+                Icon(Icons.star, color: Colors.grey, size: 30), 
+                Icon(Icons.star, color: Colors.grey, size: 30), 
+                Icon(Icons.star_border, color: Colors.grey, size: 30), 
+              ],
             ),
-            
-            Text(
-              "This is ",
-              style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 30,
-              ),
-            ),
-            Text(
-              "Flutter ",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 30,
-              ),
-            )
           ],
-        )
+        ),
       ),
     );
   }
